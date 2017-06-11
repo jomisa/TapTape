cc2c66f31465a678ee9bb590e6b353fddfaeeaff
José Miguel Sánchez <jmss10@gmail.com>
2017-06-11 01:20:49 -0500

    grabación y reproducción básica

---
+++ app/src/main/java/co/org/husi/taptape/MainActivity.java
@@ -0,0 +1,170 @@
+package co.org.husi.taptape;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AudioRecordTest";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private static String mNombreArchivo = null;
+
+    private MediaRecorder mRecorder = null;
+    private MediaPlayer mPlayer = null;
+
+    private Button mBotonGrabar = null;
+    private Button mBotonReproducir = null;
+
+    // Requesting permission to RECORD_AUDIO
+    private boolean permisoParaGrabar = false;
+    private String[] permisos = {Manifest.permission.RECORD_AUDIO};
+
+    private boolean mEmpezarGrabar;
+    private boolean mEmpezarReproducir;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permisoParaGrabar = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permisoParaGrabar) finish();
+
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        //Grabar en el directorio de cache externo para visibilidad
+        mNombreArchivo = getExternalCacheDir().getAbsolutePath();
+        mNombreArchivo += "/grabaciontest.3gp";
+
+        //Pedir permisos para grabar
+        ActivityCompat.requestPermissions(this, permisos, REQUEST_RECORD_AUDIO_PERMISSION);
+
+        LinearLayout ll = new LinearLayout(this);
+
+        mBotonGrabar = new Button(this);
+        ll.addView(mBotonGrabar, new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                0));
+
+        mBotonReproducir = new Button(this);
+        ll.addView(mBotonReproducir,
+                new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        0));
+
+        setContentView(ll);
+
+        mEmpezarGrabar = true;
+        mBotonGrabar.setText("Start recording");
+        mBotonGrabar.setOnClickListener(presionoGrabar());
+
+        mEmpezarReproducir = true;
+        mBotonReproducir.setText("Start playing");
+        mBotonReproducir.setOnClickListener(presionoReproducir());
+    }
+
+    private View.OnClickListener presionoGrabar() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                grabar(mEmpezarGrabar);
+                if (mEmpezarGrabar) {
+                    mBotonGrabar.setText("Stop recording");
+                } else {
+                    mBotonGrabar.setText("Start recording");
+                }
+                mEmpezarGrabar = !mEmpezarGrabar;
+            }
+        };
+    }
+
+    private void grabar(boolean empezar) {
+        if (empezar) {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setOutputFile(mNombreArchivo);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+
+            mRecorder.start();
+        } else {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    private View.OnClickListener presionoReproducir() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reproducir(mEmpezarReproducir);
+                if (mEmpezarReproducir) {
+                    mBotonReproducir.setText("Stop playing");
+                } else {
+                    mBotonReproducir.setText("Start playing");
+                }
+                mEmpezarReproducir = !mEmpezarReproducir;
+            }
+        };
+    }
+
+    private void reproducir(boolean empezar) {
+        if (empezar) {
+            mPlayer = new MediaPlayer();
+            try {
+                mPlayer.setDataSource(mNombreArchivo);
+                mPlayer.prepare();
+                mPlayer.start();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "prepare() failed");
+            }
+        } else {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
+
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+}
